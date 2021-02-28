b60a1632de1d5286bdeed11dd97e598d7e9577d3
zacharyranes <zachary.ranes@outlook.com>
2021-02-28 09:47:16 -0500

    Day 12

---
+++ day_12/day12_modules.py
@@ -0,0 +1,19 @@
+# main.py file
+import mymodule
+print(mymodule.generate_full_name('Asabeneh', 'Yetayeh'))
+
+from statistics import *  # importing all the statistics modules
+ages = [20, 20, 24, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))  # ~22.9
+print(median(ages))  # 23
+print(mode(ages))  # 20
+print(stdev(ages))  # ~2.3
+
+import math
+print(math.pi)  # 3.141592653589793, pi constant
+print(math.sqrt(2))  # 1.4142135623730951, square root
+print(math.pow(2, 3))  # 8.0, exponential function
+print(math.floor(9.81))  # 9, rounding to the lowest
+print(math.ceil(9.81))  # 10, rounding to the highest
+print(math.log10(100))  # 2, logarithim with 10 as base
+
---
+++ day_12/mymodule.py
@@ -0,0 +1,5 @@
+# mymodule.py file
+def generate_full_name(firstname, lastname):
+    space = ' '
+    fullname = firstname + space + lastname
+    return fullname
