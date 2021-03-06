e624062f14302184f9b867a066555ecf001c8fd7
zacharyranes <zachary.ranes@outlook.com>
2021-03-06 13:22:51 -0500

    Day 18 Done

---
+++ day_18/day18_regular-expressions.py
@@ -0,0 +1,38 @@
+import re
+
+txt = 'I love to teach python and javaScript'
+# It returns an object with span, and match
+match = re.match('I love to teach', txt, re.I)
+print(match)  # <re.Match object; span=(0, 15), match='I love to teach'>
+# We can get the starting and ending position of the match as tuple using span
+span = match.span()
+print(span)     # (0, 15)
+# Lets find the start and stop position from the span
+start, end = span
+print(start, end)  # 0, 15
+substring = txt[start:end]
+print(substring)       # I love to teach
+
+
+txt = '''Python is the most beautiful language that a human being has ever created.
+I recommend python for a first programming language'''
+
+match_replaced = re.sub('Python|python', 'JavaScript', txt, re.I)
+print(match_replaced)  # JavaScript is the most beautiful language that a human being has ever created.
+# OR
+match_replaced = re.sub('[Pp]ython', 'JavaScript', txt, re.I)
+print(match_replaced)  # JavaScript is the most beautiful language that a human being has ever created.
+
+
+regex_pattern = r'apple'
+txt = 'Apple and banana are fruits. An old cliche says an apple a day a doctor way has been replaced by a banana a day keeps the doctor far far away. '
+matches = re.findall(regex_pattern, txt)
+print(matches)  # ['apple']
+
+# To make case insensitive adding flag '
+matches = re.findall(regex_pattern, txt, re.I)
+print(matches)  # ['Apple', 'apple']
+# or we can use a set of characters method
+regex_pattern = r'[Aa]pple'  # this mean the first letter could be Apple or apple
+matches = re.findall(regex_pattern, txt)
+print(matches)  # ['Apple', 'apple']
